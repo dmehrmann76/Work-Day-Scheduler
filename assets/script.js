2fa76629e6fa2adf6cbb329684c745af1dfd7c03
Daniel Mehrmann <dmehrmann76@gmail.com>
2020-12-11 20:05:18 -0600

    js changes to time-line

--- assets/script.js
+++ assets/script.js
@@ -11,24 +11,25 @@ $(document).ready(function() {
         
     })
     //each hours text area is saved into local storage
-    $("#calendar-row9 .input").val(localStorage.getItem("calendar-row9"));
-    $("#calendar-row10 .input").val(localStorage.getItem("calendar-row10"));
-    $("#calendar-row11 .input").val(localStorage.getItem("calendar-row11"));
-    $("#calendar-row12 .input").val(localStorage.getItem("calendar-row12"));
-    $("#calendar-row1 .input").val(localStorage.getItem("calendar-row1"));
-    $("#calendar-row2 .input").val(localStorage.getItem("calendar-row2"));
-    $("#calendar-row3 .input").val(localStorage.getItem("calendar-row3"));
-    $("#calendar-row4 .input").val(localStorage.getItem("calendar-row4"));
-    $("#calendar-row5 .input").val(localStorage.getItem("calendar-row5"));
+    $("#time9 .input").val(localStorage.getItem("time9"));
+    $("#time10 .input").val(localStorage.getItem("time10"));
+    $("#time11 .input").val(localStorage.getItem("time11"));
+    $("#time12 .input").val(localStorage.getItem("time12"));
+    $("#time1 .input").val(localStorage.getItem("time1"));
+    $("#time2 .input").val(localStorage.getItem("time2"));
+    $("#time3 .input").val(localStorage.getItem("time3"));
+    $("#time4 .input").val(localStorage.getItem("time4"));
+    $("#time5 .input").val(localStorage.getItem("time5"));
 
+
+    //function that checks the current time
     function hourCheck() {
         var currentTime = moment().hour();
 
         $(".time-line").each(function() {
-            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( hourBlock, currentTime)
+            var hourBlock = parseInt($(this).attr("id").split("time")[1]);
 
-
+            // an if else statement that assigns a css class to the current hour div for correct color code 
             if (hourBlock < currentTime) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
